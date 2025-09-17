28fb53ae2f96425938feeddaf27026c724b4699b
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2025-09-17 06:16:51 +0530

    check auth updated

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -17,7 +17,7 @@ export const AuthProvider = ({children}) => {
 
     const checkAuth = async () => {
         try{
-            const {data} = await axios.post("/api/auth/check");
+            const {data} = await axios.get("/api/auth/check");
             if(data.success){
                 setAuthUser(data.user)
                 connectSocket(data.user);
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.post("/update-profile", protectRoutes, updateProfile);
-userRouter.post("/check", protectRoutes, checkAuth);
+userRouter.get("/check", protectRoutes, checkAuth);
 
 export default userRouter;
