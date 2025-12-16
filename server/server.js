3ea6372c779946b15fecd6d65bee0da5ae0ea8d8
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2025-12-16 20:20:06 +0530

    changed cors origin

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
